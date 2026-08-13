0835a8d6283fb841b12215168a7d7ef98ef90589
Toye Salami <toyejsalami@gmail.com>
2026-08-13 17:56:35 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s)-1
+
+        while l < r:
+            if s[l].lower() != s[r].lower():
+                if not (s[l].isalnum()):
+                    l +=1
+                    continue
+                if not (s[r].isalnum()):
+                    r -=1
+                    continue
+                return False
+            l +=1
+            r -=1
+                
+        return True
+        
+        
